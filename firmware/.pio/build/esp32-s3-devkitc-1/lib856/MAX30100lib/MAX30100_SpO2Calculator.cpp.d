a34099f6d8fae1c99ee88c43f0e400339e017cc4
kananjethwani24 <kanan.jethwani@gmail.com>
2026-07-11 18:14:16 +0530

    Remove build artifacts, add .gitignore rules

--- firmware/.pio/build/esp32-s3-devkitc-1/lib856/MAX30100lib/MAX30100_SpO2Calculator.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s3-devkitc-1/lib856/MAX30100lib/MAX30100_SpO2Calculator.cpp.o: \
- .pio/libdeps/esp32-s3-devkitc-1/MAX30100lib/src/MAX30100_SpO2Calculator.cpp \
- .pio/libdeps/esp32-s3-devkitc-1/MAX30100lib/src/MAX30100_SpO2Calculator.h
